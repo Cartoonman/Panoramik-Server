85885721b2c4b7ed0f6ead1ceb6353a3c2306e1c
Dong Liang <dongliang3571@gmail.com>
2017-02-21 01:07:01 -0500

    added upload file feature

--- src/app.py
+++ src/app.py
@@ -1,11 +1,56 @@
-from flask import Flask
+import os
+
+from flask import Flask, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
 import cv2
+
+from utils import allowed_file
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.debug = True
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route("/uploaded_file")
+def uploaded_file():
+    return "uploaded file successfully"
 
 if __name__ == "__main__":
     app.run()
